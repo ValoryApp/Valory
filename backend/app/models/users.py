2da84517060bdf7486821861e0fc8650d97b9350
Misha Gusev <majikx.prod0@gmail.com>
2025-02-04 06:22:23 +0500

    feat(backend): add twitch id, display name, access token and refresh token in db user model

--- backend/app/models/users.py
+++ backend/app/models/users.py
@@ -7,18 +7,21 @@ from sqlalchemy import text
 
 class UserBase(SQLModel):
     username: str = Field(default=None, nullable=False)
-    email: str = Field(default=None, nullable=False)
-
     avatar_url: str = Field(default=None, nullable=True)
+
+    twitch_id: str = Field(default=None, nullable=True)
+    twitch_display_name: str = Field(default=None, nullable=True)
 
     riot_id: str = Field(default=None, nullable=True)
     hdev_api_key: str = Field(default=None, nullable=True)
+
+    twitch_access_token: str = Field(default=None, nullable=True)
+    twitch_refresh_token: str = Field(default=None, nullable=True)
 
     created_at: datetime = Field(
         default_factory=lambda: datetime.now(),
         sa_column_kwargs={"server_default": text("current_timestamp(0)")},
     )
-
     updated_at: datetime = Field(
         default_factory=lambda: datetime.now(),
         sa_column_kwargs={
