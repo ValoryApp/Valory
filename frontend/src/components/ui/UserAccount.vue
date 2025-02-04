9eb6ef6126ea7d6388bc71e499e7d5cc8baebf9c
Misha Gusev <majikx.prod0@gmail.com>
2025-02-04 07:17:34 +0500

    feat(frontend): add user menu

---
+++ frontend/src/components/ui/UserAccount.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="user_account">
+    <div class="avatar">
+      <img width="40px" src="https://static-cdn.jtvnw.net/jtv_user_pictures/a917baec-006e-4ee8-a89a-6f8fda5b09a0-profile_image-300x300.png" alt="">
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.user_account {
+  background-color: #171717;
+  border-radius: 999px;
+
+  border: 1px solid hsla(222, 6%, 30%, 0.25);
+
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  gap: 12px;
+
+  position: fixed;
+  top: 30px;
+  right: 30px;
+
+  .avatar {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+
+    img {
+      border-radius: 100%;
+    }
+  }
+}
+</style>
