147e5d0b7cc6e8ae3b1b100ec642d7f3821246e6
Misha Gusev <majikx.prod0@gmail.com>
2025-02-13 19:48:22 +0500

    feat(backend): update add func for BaseDAO

--- backend/app/dao/base.py
+++ backend/app/dao/base.py
@@ -24,7 +24,7 @@ class BaseDAO:
 
     @classmethod
     async def add(cls, session, **data):
-        query = insert(cls.model).values(**data)
-        await session.execute(query)
+        query = insert(cls.model).values(**data).returning(cls.model)
+        result = await session.execute(query)
         await session.commit()
-        return query
+        return result.scalars().first()
