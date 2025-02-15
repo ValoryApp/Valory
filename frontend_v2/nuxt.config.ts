2d3ec026eb12f089cc5f723f2804e4017bfdc667
Misha Gusev <majikx.prod0@gmail.com>
2025-02-15 22:05:12 +0500

    feat(frontend): frontend v2

---
+++ frontend_v2/nuxt.config.ts
@@ -0,0 +1,52 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: false },
+
+  css: ['@/assets/css/main.css'],
+
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "@vueuse/nuxt",
+    "@nuxt/icon",
+    "@nuxt/fonts",
+    '@nuxtjs/google-fonts',
+    "@nuxt/image",
+    "v-wave/nuxt",
+  ],
+
+  googleFonts: {
+    families: {
+      Inter: '200..900',
+    },
+    prefetch: true,
+    preconnect: true,
+    preload: true,
+    download: true,
+    inject: true,
+  },
+
+  tailwindcss: {
+    exposeConfig: true,
+    editorSupport: true,
+  },
+
+  colorMode: {
+    classSuffix: "",
+  },
+
+  imports: {
+    imports: [
+      {
+        from: "tailwind-variants",
+        name: "tv",
+      },
+      {
+        from: "tailwind-variants",
+        name: "VariantProps",
+        type: true,
+      },
+    ],
+  },
+});
