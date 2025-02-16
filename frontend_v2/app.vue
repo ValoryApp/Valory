4ec01affcaa1e2a421d9d7f59cbb737955004ea8
Misha Gusev <majikx.prod0@gmail.com>
2025-02-17 00:48:52 +0500

    feat(frontend): update meta tags

--- frontend_v2/app.vue
+++ frontend_v2/app.vue
@@ -1,20 +1,34 @@
 <script setup lang="ts">
+
+const metaImg = 'images/meta.webp'
+const metaTitle = 'Stream Overlays - VALORY'
+const metaDescription = 'Elevate your Valorant streaming experience by using VALORY. Keep your viewers engaged and informed, and showcase your progress in the game while making your stream more captivating and memorable!'
+
 useSeoMeta({
-  title: 'Stream Overlays - VALORY',
-  description:
-    'Elevate your Valorant streaming experience by using VALORY. Keep your viewers engaged and informed, and showcase your progress in the game while making your stream more captivating and memorable!',
-  ogTitle: 'Stream Overlays - VALORY',
-  ogDescription:
-    'Elevate your Valorant streaming experience by using VALORY. Keep your viewers engaged and informed, and showcase your progress in the game while making your stream more captivating and memorable!',
-  ogImage: 'img/meta.webp',
+  title: metaTitle,
+  description: metaDescription,
+  ogTitle: metaTitle,
+  ogDescription: metaDescription,
+  ogImage: metaImg,
   twitterCard: 'summary_large_image',
-  twitterTitle: 'Stream Overlays - VALORY',
-  twitterDescription:
-    'Elevate your Valorant streaming experience by using Valory. Keep your viewers engaged and informed, and showcase your progress in the game while making your stream more captivating and memorable!',
-  twitterImage: 'image/meta.webp',
+  twitterTitle: metaTitle,
+  twitterDescription: metaDescription,
+  twitterImage: metaImg,
   author: 'MAGICX, misha@valory.su',
   keywords:
-    'valory, valory tracker, valory obs, valory obs tracker, valory overlay, obs tracker, obs overlay, valory valorant, valorant, valorant obs, valorant tracker, valorant overlay, valory obs overlay, valory valorant overlay, valory valorant obs, valorant tracker, valorant tracker, valorant stream overlay, valorant player tracker, custom display for valorant, valorant leaderboard overlay, radiant rank tracker, winrate tracker for streamers, valorant win/lose display, custom valorant leaderboard, valorant obs overlay, radiant players tracker, valorant stream custom display, streamer winrate overlay, win/lose tracker for valorant, valorant rank display, valorant custom rank overlay, valorant stream leaderboard, radiant rank winrate, streamer valorant overlay, winrate display for valorant players',
+    'valory, valory tracker, valory obs, valory obs tracker, valory overlay, ' +
+    'obs tracker, obs overlay, valory valorant, valorant, valorant obs, ' +
+    'valorant tracker, valorant overlay, valory obs overlay,' +
+    ' valory valorant overlay, valory valorant obs, valorant tracker, ' +
+    'valorant tracker, valorant stream overlay, valorant player tracker, ' +
+    'custom display for valorant, valorant leaderboard overlay, ' +
+    'radiant rank tracker, winrate tracker for streamers, ' +
+    'valorant win/lose display, custom valorant leaderboard, ' +
+    'valorant obs overlay, radiant players tracker, valorant stream ' +
+    'custom display, streamer winrate overlay, win/lose tracker for valorant, ' +
+    'valorant rank display, valorant custom rank overlay, valorant stream ' +
+    'leaderboard, radiant rank winrate, streamer valorant overlay, ' +
+    'winrate display for valorant players',
 });
 </script>
 
